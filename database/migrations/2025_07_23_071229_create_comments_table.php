ff470fafc8b519933149425149345a0a4f70e82f
Antony A <qlixess@gmail.com>
2025-07-25 04:28:40 +0700

    fix: add missing softDeletes

--- database/migrations/2025_07_23_071229_create_comments_table.php
+++ database/migrations/2025_07_23_071229_create_comments_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->foreign("post_id")->references("id")->on("posts")->onDelete("restrict");
             $table->string("content");
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
