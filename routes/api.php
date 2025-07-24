02d0ec58f0e3adf1f688d0134eb1772f7af8773f
Antony A <qlixess@gmail.com>
2025-07-25 04:28:50 +0700

    fix: update routes name

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Rebing\GraphQL\GraphQLController;
 
 Route::group(["prefix"  => "graphql"], function () {
-    Route::get("/", [GraphQLController::class, "query"])->name("graphql.get");
-    Route::post("/", [GraphQLController::class, "query"])->name("graphql.post");
-    Route::patch("/", [GraphQLController::class, "query"])->name("graphql.patch");
-    Route::delete("/", [GraphQLController::class, "query"])->name("graphql.delete");
+    Route::get("/", [GraphQLController::class, "query"])->name("graphql");
+    Route::post("/", [GraphQLController::class, "query"])->name("graphql");
+    Route::patch("/", [GraphQLController::class, "query"])->name("graphql");
+    Route::delete("/", [GraphQLController::class, "query"])->name("graphql");
 });
